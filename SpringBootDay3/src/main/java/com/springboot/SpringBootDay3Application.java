2a7ddd25ec2d03089cb1289ea0f5c0e7d72ee89c
unknown <priyakushwaha1407@gmail.com>
2026-03-23 22:32:25 +0530

    commit

---
+++ SpringBootDay3/src/main/java/com/springboot/SpringBootDay3Application.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay3Application.class, args);
+	}
+
+}
